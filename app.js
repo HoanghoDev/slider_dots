389c8a335c281c4dadc1a14240d823cb5e0105d9
hohoang <hohoang.dev@gmail.com>
2023-04-19 13:36:47 +0700

    update

--- app.js
+++ app.js
@@ -14,17 +14,27 @@ prev.onclick = function(){
     active = active - 1 >= 0 ? active - 1 : lengthItems;
     reloadSlider();
 }
+let refreshInterval = setInterval(()=> {next.click()}, 3000);
 function reloadSlider(){
     slider.style.left = -items[active].offsetLeft + 'px';
     // 
     let last_active_dot = document.querySelector('.slider .dots li.active');
     last_active_dot.classList.remove('active');
     dots[active].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 3000);
+
+    
 }
 
 dots.forEach((li, key) => {
-    
+    li.addEventListener('click', ()=>{
+         active = key;
+         reloadSlider();
+    })
 })
 window.onresize = function(event) {
     reloadSlider();
 };
+
